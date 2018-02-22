344b4de6683dfd3a1359cc264bbf039e2939f5b2
AWilliams18 <williaan18@up.edu>
2018-02-22 15:53:23 -0800

    added singleCheck()

--- sudokuChecker.cpp
+++ sudokuChecker.cpp
@@ -1,12 +1,41 @@
 // Hey here it is!
 
-import "stdlib.h"
+import<thread>
 
 
 
 class sudokuChecker {
 
 
+    bool sudokuCheck(int[][] puzzleGrid) {
+        // trust its a 9x9 array
 
-    
+        for(int i = 0; )
+
+
+
+    }
+
+
+    /* singleCheck()
+        checks that all integers 1 through 9 are
+        present in the given array.
+    */
+    bool singleCheck(int[] single) {
+        int checkList[9] = {0};
+
+        for (int i = 0; i < 9; i++) {
+            checkList[single[i]]++;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (checkList[i] != 1) {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }//singleCheck
+
 }
