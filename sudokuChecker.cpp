0186fe68f5e5f23e3d665e28e05d16ffc8f0be2f
AWilliams18 <williaan18@up.edu>
2018-03-05 18:32:29 -0800

    update with header

--- sudokuChecker.cpp
+++ sudokuChecker.cpp
@@ -1,41 +1,73 @@
 // Hey here it is!
 
-import<thread>
+#import "sudokuChecker.h"
+
+#import <thread>
+#import <string>
+
+using namespace std;
 
 
 
 class sudokuChecker {
 
+public:
+    static const int SUDOKU_SIZE = 9;
+    int main(int , const char* []);
+    bool singleCheck(int []) noexcept;
+    bool sudokuCheck(int [][9]);
+};
 
-    bool sudokuCheck(int[][] puzzleGrid) {
-        // trust its a 9x9 array
-
-        for(int i = 0; )
+sudokuChecker::sudokuChecker() {
+}
 
 
+int sudokuChecker::main(int argc, const char* argv[]) {
 
+    int testPuzzle[SUDOKU_SIZE][SUDOKU_SIZE] = {};
+    for (int i = 0; i < SUDOKU_SIZE; i++) {
+        for (int j = 0; j < SUDOKU_SIZE; j++) {
+            testPuzzle[i][j] = i;
+        }
     }
 
+    sudokuCheck(testPuzzle);
 
-    /* singleCheck()
-        checks that all integers 1 through 9 are
-        present in the given array.
-    */
-    bool singleCheck(int[] single) {
-        int checkList[9] = {0};
+    return 1;
+}
 
-        for (int i = 0; i < 9; i++) {
-            checkList[single[i]]++;
+
+
+/* singleCheck()
+    checks that all integers 1 through 9 are
+    present in the given array.
+*/
+bool sudokuChecker::singleCheck(int single[]) {
+    int checkList[9] = {0};
+
+    for (int i = 0; i < 9; i++) {
+        checkList[single[i]]++;
+    }
+
+    for (int i = 0; i < 9; i++) {
+        if (checkList[i] != 1) {
+            return false;
         }
+    }
 
-        for (int i = 0; i < 9; i++) {
-            if (checkList[i] != 1) {
-                return false;
-            }
-        }
+    return true;
 
-        return true;
-        
-    }//singleCheck
+}//singleCheck
 
+bool sudokuChecker::sudokuCheck(int puzzleGrid[][9]) {
+    // trust its a 9x9 array
+
+    std::thread threadOne(&sudokuChecker::singleCheck, puzzleGrid[0]);
+
+    threadOne.join();
+
+    // if (x) {
+    //     return 0;
+    // }
+    return true;
 }
---
+++ sudokuChecker.h
@@ -0,0 +1,12 @@
+
+#import <thread>
+#import <string>
+
+using namespace std;
+
+// class sudokuChecker {
+// public:
+//     int main(int argc, const char* argv[]);
+//     bool sudokuCheck(int puzzleGrid[][9]);
+//     bool singleCheck(int single[]);
+// };
