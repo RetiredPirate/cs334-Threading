b84932186cd02f2ca9e96d1b114f8b5f50a14592
AWilliams18 <williaan18@up.edu>
2018-03-06 15:30:13 -0800

    Final version, three threads

--- sudokuChecker.cpp
+++ sudokuChecker.cpp
@@ -1,6 +1,6 @@
 // Hey here it is!
 
-#import "sudokuChecker.h"
+//#import "sudokuChecker.h"
 
 #import <pthread.h>
 #import <string>
@@ -10,74 +10,207 @@
 using namespace std;
 
 
-#define SUDOKU_SIZE 9;
+#define SUDOKU_SIZE 9
 
+#ifndef INVALID
+#define INVALID 0
+#endif
 
-sudokuChecker::sudokuChecker() {
-}
-
-
-int main() {
-
-    int testPuzzle[SUDOKU_SIZE][SUDOKU_SIZE] = {};
-    int i;
-    for (i = 0; i < SUDOKU_SIZE; i++) {
-        for (int j = 0; j < SUDOKU_SIZE; j++) {
-            testPuzzle[i][j] = i;
-        }
-    }
-
-    sudokuCheck(testPuzzle);
-
-    return 1;
-}
+int **testPuzzle;
+bool isValid;
 
 
 
-/* singleCheck()
+/* rowCheck()
     checks that all integers 1 through 9 are
     present in the given array.
 */
-bool sudokuChecker::singleCheck(int single[]) {
-    int checkList[9] = {0};
+void *rowCheck(void* _index) {
 
-    for (int i = 0; i < 9; i++) {
-        checkList[single[i]]++;
-    }
+    for (int index = 0; index < SUDOKU_SIZE; index++) {
 
-    for (int i = 0; i < 9; i++) {
-        if (checkList[i] != 1) {
-            return false;
+        int checkList[SUDOKU_SIZE] = {0};
+
+        for (int i = 0; i < SUDOKU_SIZE; i++) {
+            checkList[testPuzzle[i][index]-1]++;
         }
+
+        for (int i = 0; i < SUDOKU_SIZE; i++) {
+            if (checkList[i] != 1) {
+                isValid = false;
+                cout << "ROW";
+                return NULL;
+            }
+        }
+
     }
 
-    return true;
 
+    return NULL;
 }//singleCheck
 
-bool sudokuChecker::sudokuCheck(int puzzleGrid[][9]) {
-    // trust its a 9x9 array
 
-    pthread_t threads[1];
-    int rc;
-    int i;
 
-    for (i = 0; i < 1; i++) {
-        cout << "Creating thread.";
-        rc = pthread_create(&threads[i], NULL, sudokuChecker::singleCheck, puzzleGrid[0]);
+/* columnCheck()
+    checks that all integers 1 through 9 are
+    present in the given array.
+*/
+void *columnCheck(void* _index) {
 
-        if (rc) {
-            cout << "failed.";
-            exit(-1);
+    for (int index = 0; index < SUDOKU_SIZE; index++) {
+
+        int checkList[SUDOKU_SIZE] = {0};
+
+        for (int i = 0; i < SUDOKU_SIZE; i++) {
+            checkList[testPuzzle[index][i]-1]++;
+        }
+
+        for (int i = 0; i < SUDOKU_SIZE; i++) {
+            if (checkList[i] != 1) {
+                isValid = false;
+                cout << "COL";
+                return NULL;
+            }
+        }
+
+    }
+
+
+    return NULL;
+}//singleCheck
+
+
+
+/* columnCheck()
+    checks that all integers 1 through 9 are
+    present in the given array.
+*/
+void *boxCheck(void* _index) {
+
+    for (int indexI = 0; indexI < 3; indexI++) {
+        for (int indexJ = 0; indexJ < 3; indexJ++) {
+
+            int checkList[SUDOKU_SIZE] = {0};
+
+            for (int i = 3*indexI; i < 3*(indexI+1); i++) {
+                for (int j = 3*indexJ; j < 3*(indexJ+1); j++) {
+                    checkList[testPuzzle[i][j]-1]++;
+                }
+            }
+
+            for (int i = 0; i < SUDOKU_SIZE; i++) {
+                if (checkList[i] != 1) {
+                    isValid = false;
+                    cout << "BOX";
+                    return NULL;
+                }
+            }
+
         }
     }
 
-    // threadOne.join();
 
-    // if (x) {
-    //     return 0;
-    // }
-pthread_exit(NULL);
+    return NULL;
+}//singleCheck
 
-    return true;
+
+
+/* sudokuCheck()
+    spawns a thread to check the validity of each row, column and box
+*/
+void sudokuCheck() {
+
+    pthread_t threads[3];
+
+    cout << "Creating thread: " << 0 << endl;
+    pthread_create(&threads[0], NULL, rowCheck, NULL);
+
+    cout << "Creating thread: " << 1 << endl;
+    pthread_create(&threads[1], NULL, columnCheck, NULL);
+
+    cout << "Creating thread: " << 2 << endl;
+    pthread_create(&threads[2], NULL, boxCheck, NULL);
+
+
+    for (int i = 0; i < SUDOKU_SIZE; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+}
+
+/* main()
+    Creates a valid or an invalid table and calls sudokuCheck()
+        to spawn threads and check the puzzle validity.
+*/
+int main() {
+
+    // Only create one puzzle at a time
+
+    testPuzzle = new int*[SUDOKU_SIZE];
+    for (int i = 0; i < SUDOKU_SIZE; i++)  {
+        testPuzzle[i] = new int[SUDOKU_SIZE];
+    }
+
+    for (int i = 0; i < SUDOKU_SIZE; i++) {
+        for (int j = 0; j < SUDOKU_SIZE; j++) {
+            if (i == 0) {
+                testPuzzle[i][j] = j+1;
+            }
+            if (i == 1) {
+                testPuzzle[i][j] = (j+3)%9+1;
+            }
+            if (i == 2) {
+                testPuzzle[i][j] = (j+6)%9+1;
+            }
+            if (i == 3) {
+                testPuzzle[i][j] = (j+1)%9+1;
+            }
+            if (i == 4) {
+                testPuzzle[i][j] = (j+4)%9+1;
+            }
+            if (i == 5) {
+                testPuzzle[i][j] = (j+7)%9+1;
+            }
+            if (i == 6) {
+                testPuzzle[i][j] = (j+2)%9+1;
+            }
+            if (i == 7) {
+                testPuzzle[i][j] = (j+5)%9+1;
+            }
+            if (i == 8) {
+                testPuzzle[i][j] = (j+8)%9+1;
+            }
+        }
+    }
+
+#if INVALID
+    testPuzzle[0][0] = 5;
+#endif
+
+    // print puzzle to make sure its valid
+    for (int i = 0; i < SUDOKU_SIZE; i++) {
+        for (int j = 0; j < SUDOKU_SIZE; j++) {
+            cout << testPuzzle[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    isValid = true;
+    // puzzle will be marked invalid if it fails a test, valid by default
+
+    sudokuCheck(); //spawns threads
+
+    delete testPuzzle;
+    string validity;
+
+    if (isValid) {
+        validity = "valid";
+    }
+    else {
+        validity = "invalid";
+    }
+
+    cout << endl << "The puzzle is " << validity << "!" << endl;
+
+    return 1;
 }
--- sudokuChecker.h
+++ sudokuChecker.h
@@ -1,13 +1,4 @@
 
-#import <thread>
-#import <string>
-
-using namespace std;
-
-class sudokuChecker {
-
-public:
-    int main(int , const char* []);
-    bool singleCheck(int []);
-    bool sudokuCheck(int [][9]);
-};
+int main();
+void *singleCheck(int []);
+bool sudokuCheck(int [][9]);
